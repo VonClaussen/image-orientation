c1c5b48110fce38ece76481e95667ee451d36f49
VonClaussen <total_egal-dan2@web.de>
2023-10-21 12:31:55 +0000

    Erster Radio button

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.write('Hello, *World!* :sunglasses:')
+
+genre = st.radio(
+    "Background of labels",
+    ["transparent", "black", "white"]
+)
+    
+background_color = None
+
+if genre == 'transparent':
+    background_color = (0,0,0,0)
+    st.write(f'Selected Background is {background_color}')
+elif genre == 'black':
+    background_color = (0,0,0,255)
+    st.write(f'Selected Background is {background_color}')
+elif genre == 'white':
+    background_color = (255,255,255,255)
+    st.write(f'Selected Background is {background_color}')
+else:
+    st.write('Please select background color')
