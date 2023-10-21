0b6a8f6ceed26a8113aaff1c137198cd34495565
VonClaussen <total_egal-dan2@web.de>
2023-10-21 13:45:05 +0000

    Upload implement

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import streamlit as st
+import pandas as pd
+from io import StringIO
+import os
+from PIL import Image, ImageDraw, ImageFont
+import functions as fx
 
-st.write('Hello, *World!* :sunglasses:')
+st.title('App to combine images and plots into one figure')
+st.subheader('Developed by Daniel Rockel 2023')
+
+background_color = None
 
 background_color_radio = st.radio(
     "Background of labels",
     ["transparent", "black", "white"]
 )
-    
-background_color = None
+
+big_image_output=st.toggle('Export final image in full size (could end up in huge files)')
+#st.write(f'{big_image_output}')    
 
 if background_color_radio == 'transparent':
     background_color = (0,0,0,0)
     st.write(f'Selected Background is {background_color}')
+    flag_background_color=0
 elif background_color_radio == 'black':
     background_color = (0,0,0,255)
     st.write(f'Selected Background is {background_color}')
@@ -19,4 +29,20 @@ elif background_color_radio == 'white':
     background_color = (255,255,255,255)
     st.write(f'Selected Background is {background_color}')
 else:
-    st.write('Please select background color')
+    flag_background_color=1
+
+
+st.divider()
+
+if flag_background_color==1:
+    st.write('Fill in background color')
+
+st.divider()
+
+uploaded_files = st.file_uploader("Upload Files", type=["jpg", "png", "jpeg", "tif"], accept_multiple_files=True)
+
+if uploaded_files:
+    for file in uploaded_files:
+        with open(os.path.join("images", file.name), "wb") as f:
+            f.write(file.read())
+        st.write(f"Saved file: {file.name}")
