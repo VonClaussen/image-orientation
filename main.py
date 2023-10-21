befaf63176d76fcd17350edf57830600e5d31b2e
VonClaussen <total_egal-dan2@web.de>
2023-10-21 12:32:19 +0000

    Anpassung

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import streamlit as st
 
 st.write('Hello, *World!* :sunglasses:')
 
-genre = st.radio(
+background_color_radio = st.radio(
     "Background of labels",
     ["transparent", "black", "white"]
 )
     
 background_color = None
 
-if genre == 'transparent':
+if background_color_radio == 'transparent':
     background_color = (0,0,0,0)
     st.write(f'Selected Background is {background_color}')
-elif genre == 'black':
+elif background_color_radio == 'black':
     background_color = (0,0,0,255)
     st.write(f'Selected Background is {background_color}')
-elif genre == 'white':
+elif background_color_radio == 'white':
     background_color = (255,255,255,255)
     st.write(f'Selected Background is {background_color}')
 else:
