f72b206324efc1978603583974252aae7d1edf6f
VonClaussen <total_egal-dan2@web.de>
2023-10-23 09:41:33 +0000

    UI

--- ui.py
+++ ui.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
-def select_background_color():
-    background_color_radio = st.radio(
+background_color_radio = st.radio(
         "Background of labels",
         ["transparent", "black", "white"]
     )
-    if background_color_radio == 'transparent':
-        return (0, 0, 0, 0)
-    elif background_color_radio == 'black':
-        return (0, 0, 0, 255)
-    elif background_color_radio == 'white':
-        return (255, 255, 255, 255)
+if background_color_radio == 'transparent':
+    background_color= (0, 0, 0, 0)
+elif background_color_radio == 'black':
+    background_color= (0, 0, 0, 255)
+elif background_color_radio == 'white':
+    background_color= (255, 255, 255, 255)
